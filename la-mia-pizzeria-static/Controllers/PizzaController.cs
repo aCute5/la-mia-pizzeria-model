9ac0e588f7154ee6a156f7c80610e682ccbaf94a
Dario Costantini <costantini95dario@gmail.com>
2023-04-06 14:48:45 +0200

    aadding model and controller locally no database

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,20 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<PizzaModel> pizze = new List<PizzaModel>
+            {
+                new PizzaModel("Margherita", "Mozzarella, Pomodoro", 5),
+                new PizzaModel("Marinara", "Pomodoro, Origano", 3),
+                new PizzaModel("Viennese","Pomodoro Mozzarella Wurstel", 6),
+
+            };
+            return View("Index", pizze);
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/PizzaModel.cs
@@ -0,0 +1,17 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaModel
+    {
+        public string? Nome { get; set; }
+
+        public string? Descrizione { get; set; }
+
+        public int Price { get; set; }
+        public PizzaModel(string? nome, string? descrizione, int price)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Price = price;
+        }
+    }
+}
